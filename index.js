612968ac90149df99d045422fba37283b6ee1da0
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2018-08-19 00:24:56 +0200

    Fix indentation

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ const parseJpeg = require('./jpeg');
 const parseWebp = require('./webp');
 
 const clients = {
-	'http:': http,
-	'https:': https,
+  'http:': http,
+  'https:': https,
 };
 
 const imageUrl = process.argv[2];
 const { protocol, hostname, path } = url.parse(imageUrl);
 const client = clients[protocol];
 if (client) {
-	fetchImage({ client, hostname, path });
+  fetchImage({ client, hostname, path });
 } else {
-	readImage({ path });
+  readImage({ path });
 }
 
 function readImage({ path }) {
@@ -35,46 +35,46 @@ function readImage({ path }) {
       throw new Error(`Unsupported extension: ${ext}`);
   }
 
-	const body = fs.readFileSync(path);
+  const body = fs.readFileSync(path);
   console.log(parser(body));
 }
 
 function fetchImage({ client, hostname, path }) {
-	const options = {
-		hostname,
-		path,
-		method: 'GET',
-		headers: {
+  const options = {
+    hostname,
+    path,
+    method: 'GET',
+    headers: {
       'Range': 'bytes=0-1023',
       // Akamai Image Manager sends image/webp for Chrome UA.
       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
-		},
-	};
-	const req = client.request(options, (res) => {
-		console.log({ statusCode: res.statusCode, headers: res.headers });
-		if (res.statusCode >= 300) {
-			console.error(`Failed to get ${imageUrl}`);
-			res.resume();
-			return;
-		}
-		const chunks = [];
-		res.on('data', (chunk) => {
-			chunks.push(chunk);
-		});
-		res.on('end', () => {
-			const body = Buffer.concat(chunks);
-			const contentType = res.headers['content-type'] || '';
-			if (contentType.startsWith('image/jpeg')) {
-				console.log(parseJpeg(body));
-			} else if (contentType.startsWith('image/webp')) {
-				console.log(parseWebp(body));
-			} else {
-				throw new Error(`Unsupported content type: ${contentType}`);
-			}
-		});
-	});
-	req.on('error', (err) => {
-		console.error(err);
-	});
-	req.end();
+    },
+  };
+  const req = client.request(options, (res) => {
+    console.log({ statusCode: res.statusCode, headers: res.headers });
+    if (res.statusCode >= 300) {
+      console.error(`Failed to get ${imageUrl}`);
+      res.resume();
+      return;
+    }
+    const chunks = [];
+    res.on('data', (chunk) => {
+      chunks.push(chunk);
+    });
+    res.on('end', () => {
+      const body = Buffer.concat(chunks);
+      const contentType = res.headers['content-type'] || '';
+      if (contentType.startsWith('image/jpeg')) {
+        console.log(parseJpeg(body));
+      } else if (contentType.startsWith('image/webp')) {
+        console.log(parseWebp(body));
+      } else {
+        throw new Error(`Unsupported content type: ${contentType}`);
+      }
+    });
+  });
+  req.on('error', (err) => {
+    console.error(err);
+  });
+  req.end();
 }
--- jpeg.js
+++ jpeg.js
@@ -23,27 +23,27 @@ function parseSegment(buffer, start) {
   const marker = buffer.slice(start, start + 2);
   if (marker[0] === 0xff) {
     switch (marker[1]) {
-			case 0xda:
-				return {
-					segment: {
-						marker: segmentName(marker[1]),
-						start,
-					},
-					next: start,
-				};
-    	case 0xe0:
-				return parseApp0(buffer, start);
-			case 0xc0:
-			case 0xc2:
-				return parseSOF(buffer, start);
-			default:
-				return parseOtherSegment(buffer, start);
-		}
+      case 0xda:
+        return {
+          segment: {
+            marker: segmentName(marker[1]),
+            start,
+          },
+          next: start,
+        };
+      case 0xe0:
+        return parseApp0(buffer, start);
+      case 0xc0:
+      case 0xc2:
+        return parseSOF(buffer, start);
+      default:
+        return parseOtherSegment(buffer, start);
+    }
   }
   return {
     segment: {
       marker: 'unknown',
-			start,
+      start,
       data: buffer.slice(start),
     },
     next: start,
@@ -65,7 +65,7 @@ function parseApp0(buffer, start) {
 
   const segment = {
     marker: 'app0',
-		start,
+    start,
     length,
     identifier,
     jfifVersionMajor,
@@ -85,25 +85,25 @@ function parseApp0(buffer, start) {
 }
 
 function parseSOF(buffer, start) {
-	const marker = buffer.slice(start, start + 2);
-	const length = buffer.readUInt16BE(start + 2);
-	const precision = buffer.readUInt8(start + 4);
-	const lines = buffer.readUInt16BE(start + 5);
-	const samplesPerLine = buffer.readUInt16BE(start + 7);
-	const componentsInFrame = buffer.readUInt8(start + 9);
-	const segment = {
-		marker: segmentName(marker[1]),
-		start,
-		length,
-		precision,
-		lines,
-		samplesPerLine,
-		componentsInFrame,
-	};
-	return {
-		segment,
-		next: start + 2 + length,
-	};
+  const marker = buffer.slice(start, start + 2);
+  const length = buffer.readUInt16BE(start + 2);
+  const precision = buffer.readUInt8(start + 4);
+  const lines = buffer.readUInt16BE(start + 5);
+  const samplesPerLine = buffer.readUInt16BE(start + 7);
+  const componentsInFrame = buffer.readUInt8(start + 9);
+  const segment = {
+    marker: segmentName(marker[1]),
+    start,
+    length,
+    precision,
+    lines,
+    samplesPerLine,
+    componentsInFrame,
+  };
+  return {
+    segment,
+    next: start + 2 + length,
+  };
 }
 
 function parseOtherSegment(buffer, start) {
@@ -111,7 +111,7 @@ function parseOtherSegment(buffer, start) {
   const length = buffer.readUInt16BE(start + 2);
   const segment = {
     marker: segmentName(marker[1]),
-		start,
+    start,
     length,
   };
   return {
@@ -122,27 +122,27 @@ function parseOtherSegment(buffer, start) {
 
 // http://dev.exiv2.org/projects/exiv2/wiki/The_Metadata_in_JPEG_files<Paste>
 function segmentName(marker) {
-	switch (marker) {
+  switch (marker) {
   case 0xc0:
     return 'Start of Frame (Baseline DCT)';
-	case 0xc2:
-		return 'Start of Frame (Progressive DCT)';
-	case 0xc4:
-		return 'Define Huffman Table(s)';
-	case 0xda:
-		return 'Start of Scan';
-	case 0xdb:
-		return 'Define Quantization Table(s)';
-	case 0xdd:
-		return 'Define restart interval';
-	case 0xfe:
-		return 'Comment';
-	default:
-		if (0xe0 <= marker && marker < 0xf0) {
-			const n = marker - 0xe0;
-			return `app${n}`;
-		} else {
-			return `unknown 0x${marker.toString(16)}`;
-		}
-	}
+  case 0xc2:
+    return 'Start of Frame (Progressive DCT)';
+  case 0xc4:
+    return 'Define Huffman Table(s)';
+  case 0xda:
+    return 'Start of Scan';
+  case 0xdb:
+    return 'Define Quantization Table(s)';
+  case 0xdd:
+    return 'Define restart interval';
+  case 0xfe:
+    return 'Comment';
+  default:
+    if (0xe0 <= marker && marker < 0xf0) {
+      const n = marker - 0xe0;
+      return `app${n}`;
+    } else {
+      return `unknown 0x${marker.toString(16)}`;
+    }
+  }
 }
